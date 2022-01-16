0f3ce1c063611e5fbe22fe03f8a2a3ea66b33f0f
Ido Kanner <idok@linesip.com>
2022-01-16 10:33:33 +0200

    Documenting values

--- consts.go
+++ consts.go
@@ -1,4 +1,7 @@
 package gocapng
 
+// UnsetRootID for namespace root id
 const UnsetRootID int = -1
+
+// SupportsAmbient to support new (libcap-ng) cap and not libcap
 const SupportsAmbient int = 1
